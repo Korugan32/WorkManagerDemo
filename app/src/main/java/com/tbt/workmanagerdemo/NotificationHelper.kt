c9de95678085bf4f7d30f47010249a0c7a3effa0
Korugan32 <tahacansenel@gmail.com>
2025-05-16 23:00:51 +0300

    Add function to display notifications

---
+++ app/src/main/java/com/tbt/workmanagerdemo/NotificationHelper.kt
@@ -0,0 +1,27 @@
+package com.tbt.workmanagerdemo
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+
+fun showNotification(context: Context, title: String, message: String) {
+    val channelId = "reminder_channel"
+    val notificationManager =
+        context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val channel = NotificationChannel(channelId, "Reminder", NotificationManager.IMPORTANCE_HIGH)
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    val notification = NotificationCompat.Builder(context, channelId)
+        .setContentTitle(title)
+        .setContentText(message)
+        .setSmallIcon(android.R.drawable.ic_dialog_info)
+        .setPriority(NotificationCompat.PRIORITY_HIGH)
+        .build()
+
+    notificationManager.notify(1, notification)
+}
